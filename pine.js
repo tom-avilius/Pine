1ad93dd98a66eabd2170ea629e28fdea43931b76
tom-avilius <aaarshdwivedi244475@gmail.com>
2023-06-11 14:54:53 +0530

    Added section for time, day and date.

---
+++ pine.js
@@ -0,0 +1,13 @@
+
+// getting html DOM elements
+
+// day, date time section => today
+const todaySection = document.getElementById('today');
+
+// -----------------------------------------------
+
+
+// making elements draggable below
+
+// today section
+element.draggable(todaySection, 'today');
